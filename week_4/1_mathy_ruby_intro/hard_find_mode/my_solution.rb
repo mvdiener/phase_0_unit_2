a056e95995421aa8527ad7c91115968b94b26b3f
Max Diener <mvdiener@gmail.com>
2014-05-12 20:36:32 -0700

    Added challenge solutions

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,14 +12,61 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# define method named mode that takes array as argument
+# at array index 0, count number of times that item occurs
+# add counter to secondary array
+
 
 # 2. Initial Solution
-
+ def mode(array)
+ 	mode_array = []
+ 	counter = 0
+ 	while array.length > 0
+ 		x = array.count(array[0])
+ 		if x > counter
+ 			mode_array = [array[0]]
+ 			counter = x
+ 			array.delete(array[0])
+ 		elsif x == counter
+ 			mode_array.push(array[0])
+ 			array.delete(array[0])
+ 		else
+ 			array.delete(array[0])
+ 		end
+ 	end
+ 	return mode_array
+ end
 
 
 
 # 3. Refactored Solution
 
+def mode(array)
+	mode_array = []
+	counter = 0
+	while array.length > 0
+		i = array[0]
+		x = array.count(i)
+		if x > counter
+			mode_array = [i]
+			counter = x
+			array.delete(i)
+		elsif x == counter
+			mode_array.push(i)
+			array.delete(i)
+		else
+			array.delete(i)
+		end
+	end
+	return mode_array
+end
 
 
 # 4. Reflection 
+# After just a few weeks, my retention of Ruby was all but 0. Feeling quite frustrated as I started to work this,
+# I needed to look the syntax on just about everything I wanted to use. On top of that, I was going about the problem
+# from the completely wrong angle, and nothing I was trying was working. I made myself stop and walk around, take a
+# breath and revist a few minutes later. I processed my thoughts again and came up with a much better way to do the
+# problem. I tend to be extremely hard on myself when I don't understand something, and that only makes things worse.
+# I just have to constantly remind myself that I'm still learning, and I need to do so at a pace that's right for me,
+# and not anyone else.
--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -12,14 +12,40 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# define median as a method that takes an array as an argument
+# sort the array
+# check if array length is odd or even
+# if array is even, middle number is median
+# if array is odd, add middle pair of numbers and divide by 2 to find median
+
 
 # 2. Initial Solution
 
-
+def median(array)
+	array.sort!
+	if array.length % 2 == 1
+		return array[(array.length-1)/2]
+	else
+		med1 = array[array.length/2]
+		med2 = array[(array.length/2)-1]
+		return (med1+med2)/2.to_f
+	end
+end
 
 
 # 3. Refactored Solution
 
-
+def median(array)
+	array.sort!
+	x = array.length
+	if x % 2 == 1
+		return array[(x-1)/2]
+	else
+		med1 = array[x/2]
+		med2 = array[(x/2)-1]
+		return (med1+med2)/2.to_f
+	end
+end
 
 # 4. Reflection 
+# See hard_find_mode my_solution.rb for reflection
