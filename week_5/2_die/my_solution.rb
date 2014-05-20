6868d8b5891346d0d27dbd263394bbd90d52ee6a
Max Diener <mvdiener@gmail.com>
2014-05-19 19:17:37 -0700

    Added solution and refactored solution

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -9,17 +9,33 @@
 # Output:
 # Steps:
 
+# create a new class
+# define a method which takes an array as a parameter
+# return an error if parameter is not an array
+# return an error if parameter is an empty array
+# define a method which displays the number of sides
+# define a method which randomly rolls the die
+
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+  	@labels = labels
+  	unless @labels.is_a? Array
+  		raise ArgumentError.new("Please enter an array")
+  	end
+  	unless @lables != []
+		raise ArgumentError.new("Please enter an array with something ACTUALLY in it") unless @labels != []
+	end
   end
 
   def sides
+  	return @labels.length
   end
 
   def roll
+  	return @labels.sample
   end
 end
 
@@ -27,14 +43,30 @@ end
 
 # 4. Refactored Solution
 
+class Die
+  def initialize(labels)
+  	@labels = labels
+  	raise ArgumentError.new("Please enter an array") unless @labels.is_a? Array
+	raise ArgumentError.new("Please enter an array with something ACTUALLY in it") unless @labels != []
+  end
 
+  def sides
+  	return @labels.length
+  end
+
+  def roll
+  	return @labels.sample
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+#check if die with a non array returns error
+Die.new(4) rescue $!.message == "Please enter an array"
+Die.new([]) rescue $!.message == "Please enter an array with something ACTUALLY in it"
 
 
 
