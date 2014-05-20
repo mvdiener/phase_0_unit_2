8b37c460891af4514266283e5f815b28e6e7c010
Max Diener <mvdiener@gmail.com>
2014-05-19 18:24:34 -0700

    Added reflection

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -80,3 +80,12 @@ rolls = Array.new(50000) {a.roll}.uniq.sort
 possible_values == rolls
 
 # 5. Reflection 
+# I had fun with this challenge, but it was a bit tricky at the start. After a bunch of reading on classes, I got started an
+# immediately ran into problems with my raise ArgumentError. I was trying to use an if statement instead of an unless statement.
+# In my mind, unless statements are extremely confusing because they seem like double negatives. I like to think positively, so
+# an if statement always comes first. Using an if sides < 0 statement in this situation wouldn't work, because the statement would 
+# evaluate to true, and I needed a false statement to get the error to throw. After I got all of that sorted out, everything else
+# was a breeze. I only refactored slightly, changing my random number generator from array.sample to rand(number). Using ruby's
+# benchmarking tool, I found rand(number) to be roughly 4 times faster. Remember we're talking in fractions of seconds here, but
+# every little bit helps.
+#
