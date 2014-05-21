01893cc28435bedbe187ecf8b8be2d87e59865bd
Max Diener <mvdiener@gmail.com>
2014-05-20 18:39:15 -0700

    Added solution and driver code

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -9,15 +9,35 @@
 # Output:
 # Steps:
 
+# define a class called GuessingGame
+# define an initialize method that takes an integer as an answer
+# define a method
+
 
 # 3. Initial Solution
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+  	@answer = answer
+    raise ArgumentError.new("Please enter an integer") unless @answer.is_a? Integer
   end
-  
-  # Make sure you define the other required methods, too
+  def guess(guess)
+  	@guess = guess
+  	if guess == @answer
+  		return :correct
+  	elsif guess > @answer
+  		return :high
+  	elsif guess < @answer
+  		return :low
+  	end
+  end
+  def solved?
+  	if @guess == @answer
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
 
 
@@ -32,9 +52,29 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+#check if entering a non integer properly throws error
+GuessingGame.new("test") rescue $!.message == "Please enter an integer"
 
+#check if guessing too high returns :high
+game = GuessingGame.new(50)
+game.guess(55) == :high
 
+#check if guessing too low returns :low
+game = GuessingGame.new(50)
+game.guess(45) == :low
 
+#check if guessing right answer returns :correct
+game = GuessingGame.new(50)
+game.guess(50) == :correct
 
+#check if .solved? properly returns false
+game = GuessingGame.new(50)
+game.guess(20)
+game.solved? == false
+
+#check if .solved? properly returns true
+game = GuessingGame.new(50)
+game.guess(50)
+game.solved? == true
 
 # 5. Reflection 
